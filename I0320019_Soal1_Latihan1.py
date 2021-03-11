408e489bcaa56b451541add0088f5fa84349ec17
Candrika Dewi <Candrikaa18@gmail.com>
2021-03-11 23:40:36 +0700

    Pengerjaan Tugas dan Exercise

---
+++ I0320019_Soal1_Latihan1.py
@@ -0,0 +1,14 @@
+#manampilkan informasi program
+print("Luas Persegi Panjang")
+
+#input nilai panjang
+p = float(input("Masukkan panjang persegi panjang: "))
+
+#input nilai lebar
+l = float(input("Masukkan lebar persegi panjang: "))
+
+#menghitung luas persegi panjang
+Luas_persegi_panjang = p * l
+
+#menampilkan hasil perhitungan
+print(Luas_persegi_panjang)
---
+++ I0320019_Soal3_Latihan1.py
@@ -0,0 +1,11 @@
+#Menampilkan informasi program
+print("Luas Kubus")
+
+#Memasukkan nilai sisi kubus
+sisi = float(input("Masukkan nilai sisi kubus: "))
+
+#Menghitung luas kubus
+luas_kubus = sisi ** 2
+
+#Menampilkan hasil perhitungan ke layar
+print(luas_kubus)
