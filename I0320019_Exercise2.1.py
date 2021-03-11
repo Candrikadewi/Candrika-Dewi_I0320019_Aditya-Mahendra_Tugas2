a70f21073e150fba07ade77a56201a0f9a6ea722
Candrika Dewi <Candrikaa18@gmail.com>
2021-03-11 10:46:25 +0700

    Pengerjaan Tugas dan Exercise

---
+++ I0320019_Exercise2.1.py
@@ -0,0 +1,18 @@
+#tipe data Boolean
+print(True)
+
+#tipe data string
+print("Ayo belajar Python")
+print("Belajar Python Sangat Mudah")
+
+#tipe data integer
+print(20)
+
+#tipe data float
+print(3.14)
+
+#tipe data Hexadecimal
+print(9a)
+
+#tipe data Complex
+print(5j)
